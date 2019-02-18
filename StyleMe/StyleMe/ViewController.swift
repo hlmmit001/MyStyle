dc3a673ce58403d176466b3d8051efb581c741a2
Mitch Holmes <hlmmit001@myuct.ac.za>
2019-02-18 11:36:17 +0200

    Updated spacing formatting in code. Removed functions and used a String extension instead. Removed the one-liner method.

--- StyleMe/StyleMe/ViewController.swift
+++ StyleMe/StyleMe/ViewController.swift
@@ -32,80 +32,70 @@ class ViewController: UIViewController {
             textOutputField.attributedText = analyseSpecialStrings(text: textInputField.text ?? "No text")
             resultsLabel.isHidden = false
         }
-        
     }
     
-    func analyseSpecialStrings (text: String)-> NSMutableAttributedString? {
+    func analyseSpecialStrings(text: String) -> NSMutableAttributedString? {
         
         let inputTextArray = text.components(separatedBy: " ")
         attributedText = NSMutableAttributedString.init(string: text)
         
-        if checkPangram(sentence: text){
+        if text.isPangram {
             let range = NSMakeRange(0, text.count)
             attributedText?.addAttributes([NSAttributedString.Key.font:UIFont.italicSystemFont(ofSize: 14)], range:range)
         }
         else
         {
-            for word in inputTextArray{
-                if checkEmail(word: word) {
-                    let range = getRange(text: text, word: word)
+            for word in inputTextArray {
+                if word.isEmail {
+                    let range = (text as NSString).range(of: word)
                     attributedText?.addAttributes([NSAttributedString.Key.font: UIFont.systemFont(ofSize: 15, weight: UIFont.Weight(rawValue: 3))], range: range)
                 }
-                if checkNumeric(word: word) {
-                    let range = getRange(text: text, word: word)
+                if word.isNumeric {
+                    let range = (text as NSString).range(of: word)
                     attributedText?.addAttributes([NSAttributedString.Key.underlineStyle: NSUnderlineStyle.thick.rawValue, NSAttributedString.Key.font:UIFont.systemFont(ofSize: 20)], range: range)
                 }
-                if checkPalindrome(word: word){
-                    let range = getRange(text: text, word: word)
+                if word.isPalindrome {
+                    let range = (text as NSString).range(of: word)
                     attributedText?.addAttributes([NSAttributedString.Key.backgroundColor: UIColor.yellow, NSAttributedString.Key.font:UIFont.systemFont(ofSize: 12, weight: UIFont.Weight(rawValue: 2))], range: range)
                 }
-                
             }
         }
-
         return attributedText
     }
+}
+
+extension String {
     
-    func getRange (text: String, word: String)-> NSRange{
+    var isPangram: Bool {
         
-        return (text as NSString).range(of: word)
-    }
-    
-    func checkEmail (word: String)-> Bool{
-        #warning("Need to update email validatation")
-        if word.contains("@")==true && word.contains(".")==true{
+        let characters: Set<Character> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        if characters.isSubset(of: self){
             return true
         }
         return false
     }
-    
-    func checkNumeric (word: String)-> Bool{
+    var isNumeric: Bool {
         
         let letters = CharacterSet.letters
-        let range = word.rangeOfCharacter(from: letters)
+        let range = self.rangeOfCharacter(from: letters)
         
-        if range == nil{//letters are present in the word
-            return true
-        }
-        
-        return false
-    }
-    
-    func checkPalindrome (word: String)-> Bool{
-        if word == String(word.reversed()){
+        if range == nil{
             return true
         }
         return false
     }
-    
-    func checkPangram (sentence: String)-> Bool{
-        let characters: Set<Character> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        if characters.isSubset(of: sentence){
+    var isEmail: Bool {
+        #warning("MITCH - Need to update email validatation")
+        if self.contains("@")==true && self.contains(".")==true{
             return true
         }
         return false
     }
-    //making some random changes for the new branch
-    
+    var isPalindrome: Bool {
+        if self == String(self.reversed()){
+            return true
+        }
+        return false
+    }
 }
 
